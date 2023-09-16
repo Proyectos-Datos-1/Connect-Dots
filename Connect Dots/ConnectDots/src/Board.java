46f3a6a4c8651ac93dc0fbc2e88ad127e1ad7e2e
FanboyxD <48070782+FanboyxD@users.noreply.github.com>
2023-09-15 20:02:09 -0600

    Update Board.java

--- Connect Dots/ConnectDots/src/Board.java
+++ Connect Dots/ConnectDots/src/Board.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
@@ -9,6 +10,9 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -19,14 +23,15 @@ public class Board extends Application {
     private static final int POINT_RADIUS = 10;
     private Socket socket;
     private PrintWriter out;
-    private Coordinates firstPoint = null;
+    private GameData firstPoint = null;
     private Pane backgroundPane; // Nuevo Pane para los puntos y las líneas
+    private BufferedReader in;
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Connect Dots Game");
 
@@ -39,6 +44,25 @@ public class Board extends Application {
         try {
             socket = new Socket(serverAddress, serverPort);
             out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Agregar esta línea
+            // Agregar esto después de crear el socket y el PrintWriter en el método start()
+            Thread receiveThread = new Thread(() -> {
+                try {
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        Gson gson = new Gson();
+                        GameData receivedData = gson.fromJson(inputLine, GameData.class);
+                        if ("line".equals(receivedData.getType())) {
+                            drawLineFromReceivedData(receivedData);
+                        }
+                        System.out.println("Coordenadas recibidas del servidor: " + inputLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            receiveThread.start();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -55,13 +79,12 @@ public class Board extends Application {
                 circle.setOnMouseClicked(new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent event) {
-                        Coordinates coordinates = new Coordinates(finalCol + 1, finalRow + 1);
-
                         if (firstPoint == null) {
-                            firstPoint = coordinates;
+                            firstPoint = GameData.createPointData(finalCol + 1, finalRow + 1);
                         } else {
-                            sendCoordinatesToServer(firstPoint, coordinates);
-                            drawLineBetweenPoints(firstPoint, coordinates); // Dibujar la línea en el cliente
+                            GameData secondPoint = GameData.createPointData(finalCol + 1, finalRow + 1);
+                            sendGameDataToServer(firstPoint, secondPoint);
+                            drawLineBetweenPoints(firstPoint, secondPoint); // Dibujar la línea en el cliente
                             firstPoint = null;
                         }
                     }
@@ -76,35 +99,59 @@ public class Board extends Application {
         primaryStage.show();
     }
 
-    private void sendCoordinatesToServer(Coordinates coordinates1, Coordinates coordinates2) {
+    private void sendGameDataToServer(GameData gameData1, GameData gameData2) {
         try {
             Gson gson = new Gson();
-            String jsonCoordinates = gson.toJson(new CoordinatesPair(coordinates1, coordinates2));
-            out.println(jsonCoordinates);
+
+            // Crear un objeto GameData que represente una línea
+            GameData lineData = GameData.createLineData(
+                gameData1.getX(), gameData1.getY(),
+                gameData2.getX(), gameData2.getY()
+            );
+
+            String jsonLineData = gson.toJson(lineData);
+
+            out.println(jsonLineData);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void drawLineBetweenPoints(Coordinates start, Coordinates end) {
+    private void drawLineBetweenPoints(GameData start, GameData end) {
         if (areAdjacent(start, end) && isVerticalOrHorizontal(start, end)) {
             Line line = new Line(
-                    start.getX() * 100 + 50,
-                    start.getY() * 100 + 50,
-                    end.getX() * 100 + 50,
-                    end.getY() * 100 + 50
+                start.getX() * 100 + 50,
+                start.getY() * 100 + 50,
+                end.getX() * 100 + 50,
+                end.getY() * 100 + 50
             );
-    
+
             line.setStroke(Color.BLUE);
             line.setStrokeWidth(2.0);
-    
+
             // Agrega la línea en la posición 0 para que esté en la capa inferior
             backgroundPane.getChildren().add(0, line);
         }
     }
 
+    private void drawLineFromReceivedData(GameData receivedData) {
+    int startX = receivedData.getStartX() * 100 + 50;
+    int startY = receivedData.getStartY() * 100 + 50;
+    int endX = receivedData.getEndX() * 100 + 50;
+    int endY = receivedData.getEndY() * 100 + 50;
+
+    Line line = new Line(startX, startY, endX, endY);
+    line.setStroke(Color.RED); // Puedes ajustar el color como desees
+    line.setStrokeWidth(2.0);
+
+    Platform.runLater(() -> {
+        backgroundPane.getChildren().add(0, line);
+    });
+}
+
+
     // Función para verificar si dos puntos son adyacentes
-    private boolean areAdjacent(Coordinates point1, Coordinates point2) {
+    private boolean areAdjacent(GameData point1, GameData point2) {
         int dx = Math.abs(point1.getX() - point2.getX());
         int dy = Math.abs(point1.getY() - point2.getY());
 
@@ -113,7 +160,7 @@ public class Board extends Application {
     }
 
     // Función para verificar si la línea es vertical u horizontal
-    private boolean isVerticalOrHorizontal(Coordinates start, Coordinates end) {
+    private boolean isVerticalOrHorizontal(GameData start, GameData end) {
         int dx = Math.abs(start.getX() - end.getX());
         int dy = Math.abs(start.getY() - end.getY());
 
@@ -121,7 +168,6 @@ public class Board extends Application {
         return (dx == 0 && dy == 1) || (dx == 1 && dy == 0);
     }
 
-    @Override
     public void stop() throws Exception {
         // Cierra la conexión con el servidor al detener la aplicación
         if (socket != null) {
